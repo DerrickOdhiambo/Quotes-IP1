06b6c718c54bbd2504216d53397e6401d9f65790
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-08-23 14:23:51 +0300

    create a new class object for date' author and the person who submitted the quote and display in the browser

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,16 +8,30 @@ import { Quotes } from '../quotes';
 })
 export class QuoteComponent implements OnInit {
   lifeQuotes: Quotes[] = [
-    { id: 1, quote: 'The purpose of our lives is to be happy.' },
-    {
-      id: 2,
-      quote: 'Life is what happens when you are busy making other plans',
-    },
-    { id: 3, quote: 'Get busy living or get busy dying' },
-    {
-      id: 4,
-      quote: 'You only live once, but if you do it right, once is enough',
-    },
+    new Quotes(
+      new Date(),
+      'The purpose of our lives is to be happy.',
+      'Dalai Lama',
+      'John Kimana'
+    ),
+    new Quotes(
+      new Date(),
+      ' Life is what happens when you are busy making other plans.',
+      'John Lennon',
+      'Sheila Mtu'
+    ),
+    new Quotes(
+      new Date(),
+      'Get busy living or get busy dying.',
+      'Stephen King',
+      'Derrick Odhiambo'
+    ),
+    new Quotes(
+      new Date(),
+      ' You only live once, but if you do it right, once is enough.',
+      'Mae West',
+      'Levis Juma'
+    ),
   ];
   constructor() {}
 
--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,4 +1,11 @@
 export class Quotes {
-  public id: number;
-  public quote: string;
+  showQuoteDetails: boolean;
+  constructor(
+    public date: Date,
+    public quote: string,
+    public author: string,
+    public person: string
+  ) {
+    this.showQuoteDetails = false;
+  }
 }
